8008bb9f19de75c31025db8d8f279152aa705bff
nouchka <github@katagena.com>
2017-02-06 14:48:59 +0000

    specify user

--- testRunning.sh
+++ testRunning.sh
@@ -3,14 +3,14 @@
 ##Wait for init
 sleep 120
 
-response=$(curl -u user:password --write-out %{http_code} --silent --output content.html http://localhost:5232/)
+response=$(curl -u user:password --write-out %{http_code} --silent --output content.html http://localhost:5232/user/)
 
 echo "Response code "$response
 if [ $response != "200" ]; then
 	exit 1
 fi
 
-response=$(curl -u user:wrong-password --write-out %{http_code} --silent --output content.html http://localhost:5232/)
+response=$(curl -u user:wrong-password --write-out %{http_code} --silent --output content.html http://localhost:5232/user/)
 
 echo "Response code "$response
 if [ $response != "401" ]; then
